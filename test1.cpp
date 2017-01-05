cd609e9c6af67b2063abea791f5e3bad1cdb59a5
Josh Calahan <jcalahan@gmail.com>
2017-01-05 03:58:48 +0000

    wip

--- test1.cpp
+++ test1.cpp
@@ -52,7 +52,7 @@ void matrix_transpose(int* A, int* At, int m, int n) {
     }
 }
 
-void csr_format(); // TODO
+void csr_format(int* A, int m, int n, int* values, int* IA, int* IJ); // TODO
 
 void csr_matrix_vector_product(int* Val, int* RowPtr, int* Col, int n, int* d, int* result) {
     for (int k=0; k<n; ++k)
@@ -65,7 +65,17 @@ void csr_matrix_vector_product(int* Val, int* RowPtr, int* Col, int n, int* d, i
     } 
 }
 
-void csr_matrix_vector_transpose(); // TODO
+void csr_transpose(int* A, int* IA, int* JA, int m, int n, int* At) {
+    for (int i=0; i<m*n; ++i) At[i] = 0;
+
+    for (int i=0; i<m; ++i) {
+        for (int j=IA[i]; j<IA[i+1]; ++j) {
+            int row = i;
+            int col = JA[j];
+            At[n*col+row] = A[j];
+        }
+    }
+}
 
 TEST(DenseMatrixTest, MatrixVectorProduct) { 
     // http://mathinsight.org/matrix_vector_multiplication
@@ -76,7 +86,7 @@ TEST(DenseMatrixTest, MatrixVectorProduct) {
         0, -3, 1
     };
 
-    // assert A[column_count * i * j] index scheme
+    // assert A[column_count * i + j] index scheme
     int i, j;
     i=0, j=0; ASSERT_EQ(1, A[n*i+j]);
     i=0, j=1; ASSERT_EQ(-1, A[n*i+j]);
@@ -199,16 +209,17 @@ TEST(SparseMatrixTest, MatrixVectorProduct) {
     int IA[] = { 0, 0, 2, 3, 4 }; // 0th = 0, ..., N-1th = NNZ
     int JA[] = { 0, 1, 2, 1 }; // column index of each element in A
 
-    csr_matrix_vector_product(A, IA, JA, n, x, b);
+    int bb[m] = {0};
+    csr_matrix_vector_product(A, IA, JA, m, x, bb);
 
 #ifdef VERBOSE
-    print_vector("result", b, n);
+    print_vector("result", bb, m);
 #endif
 
-    ASSERT_EQ(0, b[0]);
-    ASSERT_EQ(44, b[1]);
-    ASSERT_EQ(6, b[2]);
-    ASSERT_EQ(18, b[3]);
+    ASSERT_EQ(0, bb[0]);
+    ASSERT_EQ(44, bb[1]);
+    ASSERT_EQ(6, bb[2]);
+    ASSERT_EQ(18, bb[3]);
 }
 
 TEST(SparseMatrixTest, MatrixTranspose) { 
@@ -225,20 +236,11 @@ TEST(SparseMatrixTest, MatrixTranspose) {
     int IA[] = { 0, 0, 2, 3, 4 };
     int JA[] = { 0, 1, 2, 1 };
 
-    int Mt[m*n];
-    // TODO: compute w/ dense method for comparison
-    matrix_transpose(M, Mt, m, n);
+    int Mt[n*m];
+    matrix_transpose(M, Mt, m, n); // compute w/ dense method for comparison
 
-    int Mtt[m*n] = {0};
-    // TODO: (1) fold into csr_transpose
-    for (int i=0; i<n; ++i) {  
-        for (int j=IA[i]; j<IA[i+1]; ++j) {  
-            int row = i;
-            int col = JA[j];
-            int val = A[j];
-            Mtt[m * col + row] = val;
-        }  
-    } 
+    int Mtt[n*m];
+    csr_transpose(A, IA, JA, m, n, Mtt);
 
     for (int i=0; i<m*n; ++i) {
         ASSERT_EQ(Mt[i], Mtt[i]);
@@ -250,7 +252,7 @@ TEST(SparseMatrixTest, MatrixTranspose) {
     print_matrix("Mtt", Mtt, n, m);
 #endif
 
-    // TODO: (1) modify csr_transpose to output in csr storage
+    // TODO: modify csr_transpose to output in csr storage
     int At_expected[] = { 5, 8, 6, 3 };
     int IAt_expected[] = { 0, 1, 3, 4, 4 };
     int JAt_expected[] = { 1, 1, 3, 2 };
